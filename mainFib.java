79dd1f33f6a4f8f65bf872d2fbead2ac9bc2eb1f
Abstract <pacodejesus2006@gmail.com>
2026-09-08 20:40:09 -0600

    added more logic to those files so that the user can choose the fib position

--- mainFib.java
+++ mainFib.java
@@ -1,7 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class mainFib{
 
-    public static void main(String[] args){
-        System.out.println(fib.fibonacci(6));
+    public static void main(String[] args) throws IOException {
+    BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
 
-    }
+    System.out.println("Ingresa la posicion:");
+    int n = Integer.parseInt(entrada.readLine());
+    System.out.println("Fibonacci(" + n + ") = " + fib.fibonacci(n));        
+
 }
+    }
